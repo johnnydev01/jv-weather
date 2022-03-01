9ea17812b042fa579ffaf5e70eacbbcaea9828a1
Johnny Silva <johnnysilva.dev@gmail.com>
2022-03-01 17:59:41 -0300

    input pesquisa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '34e760d780538fedd1e29fee5d3f2c47'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '34e760d780538fedd1e29fee5d3f2c47'
 };
 
 /*
